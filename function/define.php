68c1bdebf1cd253a4598137882890b22d4590593
JayaniSumudini <msjs.sumudini@gmail.com>
2018-05-12 19:34:15 +0530

    eigen demo changes

--- function/define.php
+++ function/define.php
@@ -2,13 +2,13 @@
 
 define("HOST","localhost");
 
-//define("DBNAME","eigendem_golf_travel_route");
-//define("UNAME","eigendem_root");
-//define("PW","Password$3@");
+define("DBNAME","eigendem_golf_travel_route");
+define("UNAME","eigendem_root");
+define("PW","Password$3@");
 
-define("DBNAME","golf_travel_route");
-define("UNAME","root");
-define("PW","");
+//define("DBNAME","golf_travel_route");
+//define("UNAME","root");
+//define("PW","");
 
 ///* file path for save new projects */
 //define("ROAD","../roads/");
